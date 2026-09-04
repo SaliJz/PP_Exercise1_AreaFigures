ebd545aeaf6e62fa6c871ada59e4c43bc776d891
Dati-S2 <damaris852@gmail.com>
2026-09-03 21:46:02 -0500

    Create Circle

---
+++ PP_Semana2_Ejercicio1/PP_Semana2_Ejercicio1/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Semana2_Ejercicio1
+{
+    //public class Circle : Figure
+    //{
+    //    protected float rad;
+
+    //    public Circle(float rad)
+    //    {
+    //        this.rad = rad;
+    //        
+    //    }
+
+    //    public override float GetArea()
+    //    {
+    //        return (rad * rad) * 3.14f;
+
+    //    }
+    //}
+}
