4b4e4dd5fe158c4c0e435fe01fa755368a817fbe
Jeremy Chale <jeremykiel4@gmail.com>
2026-09-03 21:53:36 -0500

    Creating Square

---
+++ PP_Semana2_Ejercicio1/PP_Semana2_Ejercicio1/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Semana2_Ejercicio1
+{
+    public class Square : Figure
+    {
+        private float side;        
+
+        public Square(float side)
+        {
+            this.side = side;            
+        }
+
+        public override float GetArea()
+        {
+            return side * side;
+        }
+    }
+}
