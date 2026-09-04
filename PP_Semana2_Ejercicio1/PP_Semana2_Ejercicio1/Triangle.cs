2215c4cda61f7366753516873c9e12012a058ae8
Mtrx283 <g.barcelli.j@gmail.com>
2026-09-03 21:42:43 -0500

    Create Triangle

---
+++ PP_Semana2_Ejercicio1/PP_Semana2_Ejercicio1/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Figures
+{
+    class Triangle : Figures
+    {
+        private float b;
+        private float h;
+
+        public Triangle(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+        
+        public override float Area()
+        {
+            return b * h / 2;
+        }
+    }
+}
